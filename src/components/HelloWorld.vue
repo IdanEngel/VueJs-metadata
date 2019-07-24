9c07dcac5cd2fc2648c6a383a4b4b9016fb2a45c
Idan Engel <you@example.com>
2019-07-24 18:24:39 +0300

    the way it should be

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,32 @@
 <template>
-  <div class="metadata">
-    <input v-on:keyup.enter="post" type="text" v-model="url" id="url" placeholder="Please enter url" required />
-    <button v-on:click.prevent="post" >Get data</button> <br>
-    <img id="img" v-bind:src= "img" alt="sorry there is no image">
-    <p> <strong>Title </strong> - {{ title }}</p>
-    <p> <strong>Description </strong> - {{ description }}</p>
+  <div class="metadata" >
+    <input
+      v-on:keyup.enter="post"
+      type="text"
+      v-model="url"
+      id="url"
+      placeholder="Please enter your favorite links"
+      required
+    />
+    <button v-on:click.prevent="post">Get data</button>
+    <br />
+    <div v-for ="item in items" v-if="" :key="item.id">
+      <p v-if="item.url"> 
+          <strong>Your link - </strong>
+        <a  v-bind:href="item.url">
+          {{ item.url }}
+        </a>
+      </p>
+      <img  v-if="item.img" id="img" v-bind:src="item.img" alt="sorry there is no image" />
+      <p v-if="item.title">
+        <strong>Title - </strong>
+        {{ item.title }}
+      </p>
+      <p v-if="item.description"> 
+        <strong>Description - </strong>
+        {{ item.description }}
+      </p>
+    </div>
   </div>
 </template>
 
@@ -13,25 +35,24 @@ export default {
   name: "HelloWorld",
   data() {
     return {
-      url: "",
-      img: "",
-      title: "",
-      description: ""
+      items: []
     };
   },
   methods: {
     post: function() {
       this.$http
         .post("http://localhost:3000/", {
-        url: this.url
+          url: this.url
         })
-        .then( 
-          function(data) {
-         console.log(data);
-          this.img = data.body.image;
-          this.title = data.body.title;
-          this.description = data.body.description;
-          // alert(this.img);
+        .then(function(data) {
+          console.log(data);
+          this.items.push(
+            { img: data.body.image },
+            { title: data.body.title },
+            { description: data.body.description },
+            { url: data.body.url },
+          );
+          this.url =""
         });
     }
   }
@@ -40,14 +61,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  #img {
-    width: 50%;
-    height: 50%;
-    margin: 20px;
-  }
-  .meatadata {
-    display: grid;
-    grid-template-columns: 1fr, 1fr;
-    grid-template-rows: 1fr, 1fr;
-    };
+#img {
+  width: 50%;
+  height: 50%;
+  margin: 20px;
+}
+#url {
+  width:30%;
+}
 </style>
